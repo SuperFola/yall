2686dda10a51fb72d52b8f9f811889b0151d0f34
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-16 11:27:36 +0200

    enhances the lexer tests

--- src/test/scala/io/github/superfola/LexerSpec.scala
+++ src/test/scala/io/github/superfola/LexerSpec.scala
@@ -1,8 +1,23 @@
 package io.github.superfola
 
+import io.github.superfola.TestsHelper._
+import io.github.superfola.lexer._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class LexerSpec extends AnyFlatSpec with Matchers {
-
+  "A lexer" should "generate a list of tokens" in {
+    Lexer.fromString(addition) should be(Some(List(
+      LParenToken(),
+      SymbolToken("+"),
+      NumberToken("2"),
+      LParenToken(),
+      SymbolToken("*"),
+      NumberToken("3"),
+      NumberToken("4"),
+      RParenToken(),
+      RParenToken(),
+      EOFToken()
+    )))
+  }
 }
---
+++ src/test/scala/io/github/superfola/TestsHelper.scala
@@ -0,0 +1,34 @@
+package io.github.superfola
+
+object TestsHelper {
+  val addition    = "(+ 2 (* 3 4))"
+  val definition  = "(define six 6)"
+  val affectation = "(set! six 7)"
+  val condition   = "(if (not (> 3 (+ 4 2))) (* 3 6) 5)"
+  val loop: String =
+    """
+      |(while (< i 10)
+      |  (begin
+      |    (set! sum (+ 1 sum))
+      |    (set! i (+ 1 i))))
+      |""".stripMargin
+  val localVariable: String =
+    """
+      |(+
+      |  (let ((x 3) (y 2))
+      |  (* x y))
+      |  (/ 9 4))
+      |""".stripMargin
+  val functionDef: String =
+    """
+      |(define (fact n)
+      |    (if (= n 0) 1
+      |        (* n (fact (- n 1)))))
+      |(fact 8)
+      |""".stripMargin
+  val tupleDef: String =
+    """
+      |(define list (, 1 (, 2 3)))
+      |(nth 0 (nth 1 list))
+      |""".stripMargin
+}
