059336933e83fd72e34308724322bb61d5e85868
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-16 16:45:45 +0200

    continues the parser implementation, though I'm not sure of the direction I'm taking, because some code is being duplicated a lot

--- src/main/scala/io/github/superfola/parser/Node.scala
+++ src/main/scala/io/github/superfola/parser/Node.scala
@@ -4,14 +4,14 @@ import io.github.superfola.lexer.SymbolToken
 
 sealed trait Node
 
-final case class EmptyNode()                                                 extends Node
+final case class BeginNode(children: List[Node])                             extends Node
 final case class NumberNode(value: Double)                                   extends Node
 final case class SymbolNode(value: SymbolToken)                              extends Node
 final case class TupleNode(left: Node, right: Node)                          extends Node
-final case class ExpressionNode(expr: SymbolToken, children: List[Node])     extends Node
-final case class DefineNode(symbol: SymbolToken, value: Node)                extends Node
+final case class ExpressionNode(expr: SymbolNode, children: List[Node])     extends Node
+final case class DefineNode(symbol: SymbolNode, value: Node)                extends Node
 final case class LocalDefineNode(defines: List[DefineNode], expr: Node)      extends Node
-final case class FunctionNode(symbol: SymbolToken, body: Node)               extends Node
-final case class AffectationNode(symbol: SymbolToken, value: Node)           extends Node
+final case class FunctionNode(symbol: SymbolNode, body: Node)               extends Node
+final case class AffectationNode(symbol: SymbolNode, value: Node)           extends Node
 final case class ConditionNode(condition: Node, ifTrue: Node, ifFalse: Node) extends Node
 final case class LoopNode(condition: Node, body: Node)                       extends Node
--- src/main/scala/io/github/superfola/parser/Parser.scala
+++ src/main/scala/io/github/superfola/parser/Parser.scala
@@ -2,28 +2,48 @@ package io.github.superfola.parser
 
 import io.github.superfola.lexer._
 
+final case class PartialNode(node: Node, rest: List[Token])
+
 object Parser {
-  def fromTokens(tokens: List[Token]): Either[String, Node] =
-    tokens match {
+  private def createTree(list: List[Token]): Option[PartialNode] =
+    list match {
+      case LParenToken() :: SymbolToken(name) :: xs =>
+        name match {
+          case "define" =>
+            createTree(xs).flatMap { case PartialNode(SymbolNode(s), rest) =>
+              createTree(rest).map { case PartialNode(expr, rest) =>
+                PartialNode(DefineNode(SymbolNode(s), expr), rest)
+              }
+            }
+          case "set!" =>
+            createTree(xs).flatMap { case PartialNode(SymbolNode(s), rest) =>
+              createTree(rest).map { case PartialNode(expr, rest) =>
+                PartialNode(AffectationNode(SymbolNode(s), expr), rest)
+              }
+            }
+          case "if"    => Some(PartialNode(ConditionNode(???, ???, ???), xs))
+          case "while" => Some(PartialNode(LoopNode(???, ???), xs))
+          case "begin" => Some(PartialNode(BeginNode(???), xs))
+          case _       => Some(PartialNode(ExpressionNode(SymbolNode(SymbolToken(name)), ???), xs))
+        }
       case x :: xs =>
         x match {
-          case SymbolToken(value) => Right(SymbolNode(SymbolToken(value)))
+          case SymbolToken(value) => Some(PartialNode(SymbolNode(SymbolToken(value)), xs))
           case NumberToken(value) =>
-            value.toDoubleOption.map(v => NumberNode(v)) match {
-              case Some(value) => Right(value)
-              case None        => Left(s"Couldn't parse number '$value'")
-            }
-          case LParenToken() =>
-            xs.head match {
-              case SymbolToken(value) => ???
-              case NumberToken(_)     => Left("Unexpected Number after LParen")
-              case LParenToken()      => Left("Unexpected LParen after LParen")
-              case RParenToken()      => Right(EmptyNode())
-              case EOFToken()         => Left("Unexpected end of file")
-            }
-          case RParenToken() => ???
-          case EOFToken()    => ???
+            value.toDoubleOption
+              .map(d => PartialNode(NumberNode(d), xs))
+          case RParenToken() | EOFToken() => None
         }
-      case Nil => ???
+      case Nil => None
+    }
+
+  def fromTokens(tokens: List[Token]): Option[Node] =
+    createTree(tokens).flatMap { case PartialNode(node, rest) =>
+      if (rest.isEmpty) {
+        Some(node)
+      } else {
+        fromTokens(rest)
+          .map(otherNode => BeginNode(List(node, otherNode)))
+      }
     }
 }
