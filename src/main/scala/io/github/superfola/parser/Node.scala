8ed169152c87637d0dc5d7962b83f3f12645dabc
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-16 11:37:47 +0200

    adds node definitions for the parser

---
+++ src/main/scala/io/github/superfola/parser/Node.scala
@@ -0,0 +1,16 @@
+package io.github.superfola.parser
+
+import io.github.superfola.lexer.{NumberToken, SymbolToken}
+
+sealed trait Node
+
+final case class NumberNode(value: NumberToken)                              extends Node
+final case class SymbolNode(value: SymbolToken)                              extends Node
+final case class TupleNode(left: Node, right: Node)                          extends Node
+final case class ExpressionNode(expr: SymbolToken, children: List[Node])     extends Node
+final case class DefineNode(symbol: SymbolToken, value: Node)                extends Node
+final case class LocalDefineNode(defines: List[DefineNode], expr: Node)      extends Node
+final case class FunctionNode(symbol: SymbolToken, body: Node)               extends Node
+final case class AffectationNode(symbol: SymbolToken, value: Node)           extends Node
+final case class ConditionNode(condition: Node, ifTrue: Node, ifFalse: Node) extends Node
+final case class LoopNode(condition: Node, body: Node)                       extends Node
