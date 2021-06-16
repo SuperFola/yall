0ad4c1104e60f77c4ac2ce494065624c8ecc54f7
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-16 11:05:20 +0200

    adds empty tests spec

---
+++ src/test/scala/io/github/superfola/LexerSpec.scala
@@ -0,0 +1,8 @@
+package io.github.superfola
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class LexerSpec extends AnyFlatSpec with Matchers {
+
+}
---
+++ src/test/scala/io/github/superfola/ParserSpec.scala
@@ -0,0 +1,8 @@
+package io.github.superfola
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ParserSpec extends AnyFlatSpec with Matchers {
+
+}
